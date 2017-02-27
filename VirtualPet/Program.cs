2c0ceb071ee5aca24d3bce08e8fdd359edc59416
Kiracam <cdentonPD@gmail.com>
2017-02-27 14:35:21 -0500

    Project Files

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+
+        static string userPetName = String.Empty;
+        static bool introDone = false;
+
+
+        static void Main(string[] args)
+        {
+            do
+            {
+
+
+                Console.WriteLine("Virtual Spouse: Welcome ho-- what in the holy blazes is THAT thing?");
+                Console.WriteLine("Virtual Spouse: You brought home a... pet? A Camogatchi? Is that something Japanese?");
+                Console.WriteLine("Virtual Spouse: Whatever, I'm not taking care of it. What will you name it, anyways?");
+
+                userPetName = Console.ReadLine();
+
+                Console.WriteLine("Virtual Spouse: Well... just take care of your " + userPetName + ".");
+
+                introDone = true;
+
+
+            } while (introDone == false);
+
+            UserPet yourPet = new UserPet(userPetName);
+            string[] options = new string[] { "1. Feed", "2. Send Outside", "3. Play Together", "4. Do Nothing"};
+            
+            for (int i = 1; i < (i + 2); i++ )
+            {
+                Console.WriteLine("What will you do? Choose a number:");
+                Console.WriteLine("Hunger = " + yourPet.Hunger);
+                Console.WriteLine("waste = " + yourPet.Waste);
+                Console.WriteLine("Boredom = " + yourPet.Play);
+
+                foreach(string option in options)
+                {
+                    Console.WriteLine(option);
+                }
+
+                int userChoice = int.Parse(Console.ReadLine());
+                
+                switch (userChoice)
+                {
+                    case 1:
+                        Console.WriteLine("You fed " + userPetName + "a delicious meal! They seem rather happy.");
+                        yourPet.Feed();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("You took " + userPetName + "outside to relieve themself. They seem much more comfortable!");
+                        yourPet.Relieve();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("You played with " + userPetName + "for a few hours. They seem highly amused!");
+                        yourPet.Entertain();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("You decide to leave " + userPetName + "alone for now. They look quite confused.");
+                        yourPet.Nothing();
+                        break;
+
+                    default:
+                        Console.WriteLine("That is not an option. Choose one next time.");
+                        yourPet.Nothing();
+                        break;
+
+                        
+                }
+
+                yourPet.Event();
+            }
+            
+        }
+
+        
+
+       
+    }
+}
---
+++ VirtualPet/UserPet.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    public class UserPet
+    {
+        private int hunger = 100;
+        private int waste = 100;
+        private int play = 100;
+        private string petName = string.Empty; 
+        
+         
+             
+
+        public UserPet ()
+        {
+
+        }
+
+        public UserPet (string name)
+        {
+            this.petName = name;
+            
+        }
+
+        public UserPet (string name, int hungerCon, int wasteCon, int playCon)
+        {
+            this.hunger = hungerCon;
+            this.waste = wasteCon;
+            this.play = playCon;
+            this.petName = name; 
+        }
+
+        public string PetName
+        {
+            get { return petName; }
+        }
+
+        public int Hunger
+        {
+            get { return hunger;  }
+        }
+
+        public int Waste
+        {
+            get { return waste; }
+        }
+
+        public int Play
+        {
+            get { return play; }
+        }
+
+        public void Feed()
+        {
+            hunger+= 2;
+
+            waste+= 2;
+
+            play-= 2;
+
+        }
+
+        public void Relieve()
+        {
+            waste -= 2;
+
+            hunger += 2; 
+        }
+
+        public void Entertain()
+        {
+            play += 2;
+
+            hunger -=2; 
+        }
+
+        public void Nothing()
+        {
+            hunger--;
+
+            play--;
+
+            waste--;
+        }
+
+        
+
+        public void Event()
+        {
+            Random randomOne = new Random();
+            int randomFirst = randomOne.Next(1, 4);
+            Random randomTwo = new Random();
+            int randomSecond = randomTwo.Next(1, 10);
+
+            switch (randomFirst)
+            {
+                case 1:
+                    Console.WriteLine("Oh no! Your lovely " + petName + " ate some bad food! It seems to have vomitted and needs some more dinner...");
+                    hunger -= randomSecond;
+                    break;
+
+                case 2:
+                    Console.WriteLine("Oh no! Something didn't agree with " + petName + "at all! I think it might have to use the restroom very bad!");
+                    waste -= randomSecond;
+                    break;
+
+                case 3:
+                    Console.WriteLine("Oh no! " + petName + " is dying of sheer boredom! Go and have some fun already!");
+                    play -= randomSecond;
+                    break;
+            }
+
+
+
+            
+        }
+    }
+}
